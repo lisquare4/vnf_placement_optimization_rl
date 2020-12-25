466271c7b2e368f8818a6e47adafe448e9b97809
Li Li <lisquare4@gmail.com>
2020-12-26 03:42:10 +0800

    [Test]  Script to generate learning cmds

---
+++ fl_learn.py
@@ -0,0 +1,48 @@
+
+small_env = '--min_length=12 --max_length=12 --num_layers=1 --hidden_dim=32 --num_cpus=10 --env_profile="small_default" --num_epoch=10000 '
+big_env = '--min_length=20 --max_length=20 --num_layers=3 --hidden_dim=64 --num_cpus=20 --env_profile="big_default" --num_epoch=20000 '
+
+base = 'python main.py --learn_mode=true --save_model=true --num_runs=5 --weight_map="all" '
+
+trend_mode = [
+    '"reward"',
+    '"penalty"',
+]
+
+trend_coef = ['0.3', '0.4', '0.5']
+iters = ['500', '1000', '1500']
+
+cmd = ''
+
+with open('fl_script.txt', 'w') as f:
+    for env in [small_env, big_env]:
+        for it in iters:
+            for tm in trend_mode:
+                for tc in trend_coef:
+                    save_path = 's_{}_{}_{}_'.format(tc, tm.strip("'")[:2], it)
+                    if env == small_env:
+                        path_list = list(save_path)
+                        path_list[0] = 'g'
+                        save_path = ''.join(path_list)
+                    cmd = base + env + \
+                          "--trend_mode={} ".format(tm) + \
+                          "--trend_coef={} ".format(tc) + \
+                          "--cycle_iter={} ".format(it) + \
+                          "--save_to=save/{}".format(save_path)
+                    f.write(cmd + '\n')
+
+            # average
+            tm = 'ave'
+            tc = '0.5'
+            save_path = 's_{}_{}_'.format(tm, it)
+            if env == small_env:
+                path_list = list(save_path)
+                path_list[0] = 'g'
+                save_path = ''.join(path_list)
+            cmd = base + env + \
+                  "--trend_mode={} ".format(tm) + \
+                  "--trend_coef={} ".format(tc) + \
+                  "--cycle_iter={} ".format(it) + \
+                  "--save_to=save/{}".format(save_path)
+            f.write(cmd + '\n')
+            f.write('\n')
